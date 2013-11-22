5a4e37938acf8aa03faca5d42a5178bcdf832ecc
Paul Bienkowski <opatutlol@aol.com>
2013-11-22 13:40:48 +0100

    world units

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -12,4 +12,22 @@ void GameState::onUpdate(double dt) {
 }
 
 void GameState::onDraw(sf::RenderTarget& target) {
+    sf::View view;
+    float zoom = 6;
+    float w = zoom;
+    float h = w / target.getSize().x * target.getSize().y;
+    view.reset(sf::FloatRect(player->position.x-w/2, player->position.y-h/2, w, h));
+    //view.setViewport(sf::FloatRect(0.f, 0.f, 0.5f, 1.f));
+    target.setView(view);
+
+    // draw some background stuff
+    for(int x = -10; x < 10; ++x) {
+        for(int y = -10 + (x%2); y < 10; y+=2) {
+            sf::RectangleShape rect;
+            rect.setSize(sf::Vector2f(1, 1));
+            rect.setFillColor(sf::Color(100, 100, 100));
+            rect.setPosition(x, y);
+            target.draw(rect);
+        }
+    }
 }
--- src/ResourceManager.cpp
+++ src/ResourceManager.cpp
@@ -3,6 +3,7 @@
 void ResourceManager::addTexture(const std::string& name, const std::string& filename) {
     auto texture = std::make_shared<sf::Texture>();
     texture->loadFromFile(filename);
+    texture->setSmooth(true);
     m_textures[name] = texture;
 }
 
