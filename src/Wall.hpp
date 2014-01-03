5f7e3777a86ab16affacc466fdbf969359af9334
Paul Bienkowski <opatutlol@aol.com>
2014-01-03 18:44:10 +0100

    Cerealization!

--- src/Wall.hpp
+++ src/Wall.hpp
@@ -2,6 +2,9 @@
 #define WALL_HPP
 
 #include <SFML/Graphics.hpp>
+#include <cereal/types/string.hpp>
+#include <cereal/types/base_class.hpp>
+#include <cereal/types/polymorphic.hpp>
 
 #include "Entity.hpp"
 
@@ -13,10 +16,15 @@ public:
     void onUpdate(double dt) override;
     void onDraw(sf::RenderTarget& target) override;
 
+    template<class Archive>
+    void serialize(Archive& ar) {
+        ar(cereal::make_nvp("entity", cereal::base_class<Entity>(this)));
+        ar(cereal::make_nvp("m_type", m_type));
+    }
+
 private:
     std::string m_type;
     sf::Sprite m_sprite;
-
 };
 
 #endif
