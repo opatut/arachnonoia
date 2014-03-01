e8e75a81e9bdddb8e140957a70d352ff11c752c3
Paul Bienkowski <opatutlol@aol.com>
2014-03-01 17:18:53 +0100

    Spider webs instead of question marks

--- src/Pair.cpp
+++ src/Pair.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 
 #include <Thor/Math.hpp>
+#define GLM_FORCE_RADIANS
+#include <glm/gtx/vector_angle.hpp>
 
 #include "Root.hpp"
 
@@ -12,8 +14,7 @@ Pair::Pair()
     m_sprite.setTexture(* Root().resources.getTexture("pair").get());
 
     m_mass = 0.f;
-    m_physicsShape = new btSphereShape(0.5);
-    // m_physicsShape->
+    m_physicsShape = new btSphereShape(0.01);
 }
 
 std::string Pair::getTypeName() {
@@ -36,13 +37,44 @@ void Pair::onUpdate(double dt) {
 }
 
 void Pair::onDraw(sf::RenderTarget& target) {
-    if(m_solved) return;
+    glm::vec2 root(0, 0.5);
+    glm::rotate(root, m_rotation);
+    root += m_position;
+
+    for(int i = 0; i < 2; ++i) {
+        sf::ConvexShape shape(3);
+        shape.setPoint(0, sf::Vector2f(-0.02,  0));
+        shape.setPoint(1, sf::Vector2f( 0.00, -1));
+        shape.setPoint(2, sf::Vector2f( 0.02,  0));
+        shape.setFillColor(sf::Color::Black);
+        shape.setPosition(root.x, root.y);
+        shape.setRotation(m_rotation + (i-0.5) * 70);
+        target.draw(shape);
+    }
+
+    glm::vec2 center = m_position;
+    sf::Color lineColor(255, 255, 255, 120);
+    sf::VertexArray lines;
+    for(int i = 0; i < 6; ++i) {
+        glm::vec2 out = glm::rotate(glm::vec2(1, 0), i * 2.f / 6.f * thor::Pi);
+        glm::vec2 out2 = glm::rotate(glm::vec2(1, 0), (i+1) * 2.f / 6.f * thor::Pi);
+
+        lines.append(sf::Vertex(sf::Vector2f(center.x, center.y), lineColor));
+        lines.append(sf::Vertex(sf::Vector2f(center.x + out.x, center.y + out.y), lineColor));
+        for(float d = 0.1; d < 0.8; d+=0.2) {
+            lines.append(sf::Vertex(sf::Vector2f(center.x + d * out.x,  center.y + d * out.y ), lineColor));
+            lines.append(sf::Vertex(sf::Vector2f(center.x + d * out2.x, center.y + d * out2.y), lineColor));
+        }
+    }
+    lines.setPrimitiveType(sf::Lines);
+    target.draw(lines);
+
 
     m_sprite.setPosition(m_position.x, m_position.y);
     m_sprite.setScale(1/512.f, 1/512.f);
     m_sprite.setOrigin(m_sprite.getTexture()->getSize().x / 2, m_sprite.getTexture()->getSize().y / 2);
     m_sprite.setRotation(thor::toDegree(m_rotation));
-    target.draw(m_sprite);
+    // target.draw(m_sprite);
 
     if(m_active) {
         m_glyphSprite.setColor(sf::Color(255, 255, 255, fmin(255, 255 * m_activationTime)));
